1484702c440f4556579df08ccb30ccd98b3d991a
keshav0592 <bhattark92@gmail.com>
2021-02-09 00:50:18 -0500

    created delete button and design home and signin form

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,23 +1,27 @@
 import React from "react";
 import Spinner from "react-spinkit";
 import { withAsyncAction } from "../../redux/HOCs";
+import Button from "react-bootstrap/Button";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./LoginForm.css";
+import { Card } from "react-bootstrap";
 
 class LoginForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       username: "",
-      password: ""
+      password: "",
     };
   }
 
-  handleLogin = e => {
+  handleLogin = (e) => {
     e.preventDefault();
     this.props.login(this.state);
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
@@ -25,28 +29,43 @@ class LoginForm extends React.Component {
     const { loading, error } = this.props;
     return (
       <div className="LoginForm">
-        <form id="login-form" onSubmit={this.handleLogin}>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            autoFocus
-            required
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            required
-            onChange={this.handleChange}
-          />
-          <button type="submit" disabled={loading}>
-            Login
-          </button>
-        </form>
-        {loading && <Spinner name="circle" color="blue" />}
-        {error && <p style={{ color: "red" }}>{error.message}</p>}
+        <Card style={{ width: "18rem" }} bg="dark">
+          <Card.Body>
+            <Card.Title>Your favorite Microblogging platform</Card.Title>
+
+            <hr />
+
+            <form id="login-form" onSubmit={this.handleLogin}>
+              {/* <label htmlFor="username">Username</label> */}
+              <div>
+                <input
+                  type="text"
+                  name="username"
+                  autoFocus
+                  required
+                  onChange={this.handleChange}
+                  placeholder="Username"
+                />
+              </div>
+              <div>
+                {/* <label htmlFor="password">Password</label> */}
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  onChange={this.handleChange}
+                  placeholder="Password"
+                />
+              </div>
+              <hr />
+              <Button variant="info" type="submit">
+                Sign In
+              </Button>{" "}
+              {loading && <Spinner name="circle" color="blue" />}
+              {error && <p style={{ color: "red" }}>{error.message}</p>}
+            </form>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
